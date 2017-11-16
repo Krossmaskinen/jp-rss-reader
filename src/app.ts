ea4efedcb62d32b238b41a86c10707e8e6183971
Bill <bill.kurtson@gmail.com>
2017-11-16 22:30:11 +0100

    - remove unused files - remove unused routes - change project name and title

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,9 @@ export class App {
   public router: Router;
 
   public configureRouter(config: RouterConfiguration, router: Router) {
-    config.title = 'Aurelia';
+    config.title = 'JP RSS Reader';
     config.map([
-      { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome',      nav: true, title: 'Welcome' },
-      { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title: 'Github Users' },
-      { route: 'feeds', name: 'feed', moduleId: 'feed-list', nav: true, title: 'Feeds' }
+      { route: ['', 'feeds'], name: 'feed', moduleId: 'feed-list', nav: true, title: 'Feeds' }
     ]);
 
     this.router = router;
